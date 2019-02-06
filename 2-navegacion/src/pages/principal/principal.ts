1f1697bcd6fc2fed6667cd2c1b4b30053163358d
Unknown <estban35@hotmail.com>
2019-02-06 08:20:03 -0600

    Actualizacion de respaldo

--- 2-navegacion/src/pages/principal/principal.ts
+++ 2-navegacion/src/pages/principal/principal.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams , MenuController} from 'ionic-angular';
+/*import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams , MenuController, AlertController, ToastController} from 'ionic-angular';
 import {Pagina2Page} from "../index.pagina";
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
+
+
+
 /**
  * Generated class for the PrincipalPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-
+/*
 @IonicPage()
 @Component({
   selector: 'page-principal',
@@ -16,9 +20,15 @@ import {Pagina2Page} from "../index.pagina";
 export class PrincipalPage {
 
 pagina2:any = Pagina2Page;
-  constructor(public navCtrl: NavController, public navParams: NavParams, private menuCtrl: MenuController) {
+//bluetooth: any = BluetoothSerial;
+listDevices:any   = [];
+var2: any = '';
+  constructor(public navCtrl: NavController, public navParams: NavParams, private menuCtrl: MenuController,
+    private bluetoothSerial: BluetoothSerial, private alertCtrl : AlertController, private toastCtrl: ToastController) {
 
+      //this.getAlBluetoothDevices();
   }
+
 
   navegarpagina(){
     console.log("llama a navbar");
@@ -30,7 +40,126 @@ pagina2:any = Pagina2Page;
   }
 
   mostarMenu(){
-     this.menuCtrl.toggle(); 
+     this.menuCtrl.toggle();
+  }
+*/
+
+
+
+//}
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
+import { AlertController, ToastController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  pairedList: pairedlist;
+  listToggle: boolean = false;
+  pairedDeviceID: number = 0;
+  dataSend: string = "";
+
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController, private bluetoothSerial: BluetoothSerial, private toastCtrl: ToastController) {
+    this.checkBluetoothEnabled();
   }
 
+  checkBluetoothEnabled() {
+    this.bluetoothSerial.isEnabled().then(success => {
+      this.listPairedDevices();
+    }, error => {
+      this.showError("Please Enable Bluetooth")
+    });
+  }
+
+  listPairedDevices() {
+    this.bluetoothSerial.list().then(success => {
+      this.pairedList = success;
+      this.listToggle = true;
+    }, error => {
+      this.showError("Please Enable Bluetooth")
+      this.listToggle = false;
+    });
+  }
+
+  selectDevice() {
+    let connectedDevice = this.pairedList[this.pairedDeviceID];
+    if (!connectedDevice.address) {
+      this.showError('Select Paired Device to connect');
+      return;
+    }
+    let address = connectedDevice.address;
+    let name = connectedDevice.name;
+
+    this.connect(address);
+  }
+
+  connect(address) {
+    // Attempt to connect device with specified address, call app.deviceConnected if success
+    this.bluetoothSerial.connect(address).subscribe(success => {
+      this.deviceConnected();
+      this.showToast("Successfully Connected");
+    }, error => {
+      this.showError("Error:Connecting to Device");
+    });
+  }
+
+  deviceConnected() {
+    // Subscribe to data receiving as soon as the delimiter is read
+    this.bluetoothSerial.subscribe('\n').subscribe(success => {
+      this.handleData(success);
+      this.showToast("Connected Successfullly");
+    }, error => {
+      this.showError(error);
+    });
+  }
+
+  deviceDisconnected() {
+    // Unsubscribe from data receiving
+    this.bluetoothSerial.disconnect();
+    this.showToast("Device Disconnected");
+  }
+
+  handleData(data) {
+    this.showToast(data);
+  }
+
+  sendData() {
+    this.dataSend+='\n';
+    this.showToast(this.dataSend);
+
+    this.bluetoothSerial.write(this.dataSend).then(success => {
+      this.showToast(success);
+    }, error => {
+      this.showError(error)
+    });
+  }
+
+  showError(error) {
+    let alert = this.alertCtrl.create({
+      title: 'Error',
+      subTitle: error,
+      buttons: ['Dismiss']
+    });
+    alert.present();
+  }
+
+  showToast(msj) {
+    const toast = this.toastCtrl.create({
+      message: msj,
+      duration: 1000
+    });
+    toast.present();
+
+  }
+
+}
+
+interface pairedlist {
+  "class": number,
+  "id": string,
+  "address": string,
+  "name": string
 }
